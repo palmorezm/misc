dcfab2145c03b1dc3fd5884132fbf4834b8afbbd
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-03-25 14:04:51 -0400

    Update

--- JobSearch_Data_Indeed.R
+++ JobSearch_Data_Indeed.R
@@ -2,8 +2,8 @@
 # March 24, 2021
 # Scrape Indeed for Jobs Data
 
-# Link for Indeed Search
-# https://www.indeed.com/jobs?q=decision+scientist&l=Remote
+# Base link for Indeed Search
+# https://www.indeed.com/jobs?q=
 
 # Packages
 require(rvest)
@@ -11,11 +11,10 @@ require(readr)
 require(tidyverse)
 require(DT)
 
+# --------------------- Sample Scrape --------------------- # 
 
-# Sample Scrape
-# 
 #Specifying the url for desired website to be scraped
-url <- 'https://www.indeed.com/jobs?q=Data%20Science&l&vjk=a04c89acee43bb71'
+url <- 'https://www.indeed.com/jobs?q=data+science&l='
 
 #Reading the HTML code from the website
 webpage <- read_html(url)
@@ -25,13 +24,25 @@ job_data_html <- html_nodes(webpage,'.clickcard , .jobtitle')
 # The characters '.text-primary' came from selector gadget
 
 #Converting the ranking data to text
-job_data <- html_text(job_data_html)
+sample_job_data <- html_text(job_data_html)
 
 #Let's have a look at the rankings
-head(job_data)
+head(sample_job_data)
+
+# Scrub-a-dub
+sample <- read_delim(sample_job_data, "\t", quote = "\\\"", 
+                   escape_double = FALSE, col_names = FALSE, 
+                   na = "empty", trim_ws = TRUE)
+# All clean? 
+head(sample)
+
+# -------------------------- End -------------------------- #
 
 
-# Search filters
+
+
+
+# -------------------- Search Filters --------------------- #
 
 # Example:
 # https://www.indeed.com/jobs?q=decision+scientist&l=Remote&explvl=entry_level&fromage=7
@@ -48,18 +59,63 @@ head(job_data)
 # Date posted
 # https://www.indeed.com/jobs?q=decision+scientist&l=Remote&explvl=entry_level& #fromage= #*7* # Within last 7 days
 
+# -------------------------- End -------------------------- #
 
-# HTML Text Data Scrapes
 
-# Decision Scientist Scrape
 
-# Data Scientist Scrape
 
-# Data Analyst Scrape
 
-# Operations Analyst Scrape
+# ------------------- Text Data Scrapes ------------------- #
 
-# Operations Manager Scrape 
+# Title: Decision Scientist
+
+listings <- data.frame(title=character(),
+                       company=character(), 
+                       location=character(), 
+                       summary=character(), 
+                       link=character(), 
+                       description = character(),
+                       stringsAsFactors=FALSE) 
+
+for (i in seq(0, 10, 2)){
+  url_ds <- paste0('https://www.indeed.com/jobs?q=Data+Science&l=',i)
+  var <- read_html(url_ds)
+  title <-  var %>% 
+    html_nodes('#resultsCol .jobtitle') %>%
+    html_text() %>%
+    str_extract("(\\w+.+)+") 
+  summary <- var %>%
+    html_nodes('#resultsCol .summary') %>%
+    html_text() %>%
+    str_extract(".+") 
+  link <- var %>%
+    html_nodes('#resultsCol .jobtitle .turnstileLink, #resultsCol a.jobtitle') %>%
+    html_attr('href') 
+  link <- paste0("https://www.indeed.com",link)
+  location <- var %>%
+    html_nodes('#resultsCol .location') %>%
+    html_text() %>%
+    str_extract("(\\w+.)+,.[A-Z]{2}") 
+  company <- var %>% 
+    html_nodes('#resultsCol .company') %>%
+    html_text() %>%
+    str_extract("(\\w+).+")  
+  
+  listings <- rbind(listings, as.data.frame(cbind(title,
+                                                  company,
+                                                  location,
+                                                  summary,
+                                                  link)))
+}
+
+
+# Data Scientist 
+
+# Data Analyst
+
+# Operations Analyst 
+
+# Operations Manager 
 
 # [ Insert title ] ... Scrape
 
