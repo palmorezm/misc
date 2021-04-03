c3e247a4c7137a16ce648e89cec14232f5f0fcff
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-04-03 15:38:17 -0400

    Create_Review_Indeed.R

---
+++ Review_Indeed.R
@@ -0,0 +1,26 @@
+# Jobs Review
+# April 3 2021 
+# Indeed Job Postings
+
+
+
+# -------- General Industry Questions ----------- # 
+# Within the remote jobs, where are the companies actually based (headquarters)?
+# In other words, if you were to physically work for them where would it be?  
+# What is the most common place that offers remote? 
+# What about the second most common place for remote work? 
+# Where are the most jobs located? (Still around population clusters?)
+# How much do these jobs pay?
+# Who is the most frequent employer? 
+# What other titles were included in this data scrape from indeed? 
+# How much experience is required/preferred for the jobs? 
+# Was there a majority of entry, senior, or mid-level roles? 
+# What is the most common level of experience desired? 
+# What skills are they requiring? 
+# Are there certain languages that occur more than others? 
+# What soft skills are in-demand from these employers? 
+
+# ---------- Data Specific ---------- #
+# How many jobs in the data are unique listings? 
+# Which jobs are duplicates? (remove them)
+# 
