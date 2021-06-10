6a1d636a4c28b6e0b1906c47f82695d16848675d
palmorezm <palmorezm@users.noreply.github.com>
2021-06-09 22:34:01 -0400

    Update_InitialVitals.R
    Test 1 of vitals with BP, HR, weight

--- Biometrics/Vitals.R
+++ Biometrics/Vitals.R
@@ -1,2 +1,23 @@
 # Biometrics Test
-# 
+
+
+vitals <- read.csv("https://raw.githubusercontent.com/palmorezm/misc/master/Biometrics/Vitals1.csv")
+View(vitals)
+
+library(dplyr)
+head(vitals)
+
+df <- vitals %>% 
+  mutate(AVGBPS = (BPS1 + BPS2 + BPS3)/ 3,
+         AVGBPD = (BPD1 + BPD2 + BPD3)/ 3,
+         AVGHR = (HR1 + HR2 + HR3) / 3)  %>% 
+  dplyr::select(Date,
+                Weight, 
+                AVGBPS, 
+                AVGBPD, 
+                AVGHR)
+plot(df)
+summary(df)
+
+df.mod <- lm(Weight~AVGBPS + AVGBPD + AVGHR, df)
+summary(df.mod)
