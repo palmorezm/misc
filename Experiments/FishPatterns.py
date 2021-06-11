9f41d6bc66c032d8d60972ea9694b2ff5aa24688
palmorezm <palmorezm@users.noreply.github.com>
2021-06-10 20:11:30 -0400

    Create_FishPatterns.py
    Ensure data set is hosted and imported

---
+++ Experiments/FishPatterns.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 15:15:05 2021
+
+@author: Owner
+"""
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pickerel = pd.read_csv("https://raw.githubusercontent.com/palmorezm/misc/master/Experiments/Pickerel.csv")
+print(pickerel)
+
+
