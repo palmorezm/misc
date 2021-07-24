a0c20bb3b2a3c4dbe9a615a179a8c7417bb41211
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-07-23 23:19:45 -0400

    Update_Biometrics.R

--- Biometrics/Vitals.R
+++ Biometrics/Vitals.R
@@ -1,12 +1,14 @@
 # Biometrics Test
 
 library(dplyr)
-library(kableExtra)
+library(ggplot2)
+library(tidyr)
 
 vitals <- read.csv("https://raw.githubusercontent.com/palmorezm/misc/master/Biometrics/Vitals1.csv")
 
 vitals2 <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTcFzJXYjKvyhUI3my6VTaqRfWG0-pHldCRvf3nndHPIVMh-C1BqzGvB8P9p-GZZ63fbXdS4i0O8a5C/pub?output=csv")
 
+vitals <- vitals2
 
 df <- vitals %>% 
   mutate(AVGBPS = (BPS1 + BPS2 + BPS3)/ 3,
@@ -42,3 +44,23 @@ summary(df2.mod)
 
 plot(df)
 plot(df2)
+
+df[is.na(df)] <- 0
+
+
+df.excludeszero <- df %>% 
+  data.frame() %>% 
+  gather(key, value) %>% 
+  filter(key == "AVGBPS") %>%
+  mutate(Value = as.numeric(value),
+         BPS.avg = (round(Value, 1))) %>% 
+  filter(BPS.avg >1) 
+  
+df.excludeszero %>% 
+  filter(Value > 100) %>% 
+  ggplot(aes(Value, fill = key)) + 
+  geom_histogram(stat = "count", binwidth = 5) + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
