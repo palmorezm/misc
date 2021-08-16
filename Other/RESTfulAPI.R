d18cc1662d7972a789ce41a7cf2820374cc01eb0
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-08-16 19:00:29 -0400

    Create RESTfulAPI.R

---
+++ Other/RESTfulAPI.R
@@ -0,0 +1,12 @@
+
+# NOAA Climate API
+
+library(httr)
+library(jsonlite)
+library(RCurl)
+# Review code with email description
+token <- "DspzZNixzfoEhgvFBUTH"  
+# Consider the documented code 
+curl 
+curl -H "token:<token>" "url"
+$.ajax({ url:<url>, data:{<data>}, headers:{ token:<token> } })
