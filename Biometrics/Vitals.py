d612285669462562a56e98251f7cd9bea70060cd
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-12-23 12:14:03 -0500

    Create Vitals.py Layout

---
+++ Biometrics/Vitals.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data source (published publicly)
+vitals = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTcFzJXYjKvyhUI3my6VTaqRfWG0-pHldCRvf3nndHPIVMh-C1BqzGvB8P9p-GZZ63fbXdS4i0O8a5C/pub?output=csv")
+print(vitals)
+
+nas = vitals[vitals.isnull().any(axis=1)]
+print (nas)
+
+vitals[vitals.isna()]
+
+
+
