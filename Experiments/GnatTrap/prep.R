7bcaa0396aa599124084cfe4351151d2d919d1d6
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-08-01 20:48:01 -0500

    Add Link

--- Experiments/GnatTrap/prep.R
+++ Experiments/GnatTrap/prep.R
@@ -2,3 +2,4 @@
 # Gnat Trap
 # Preparation Script
 
+df <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4Y0ZRfzQLbwILHYwOJz3srSZRf3m-3V3srZ5JDfx3giq-rzE9qswEy-upWXmUa5o7poth9IENXYg/pub?output=csv")
