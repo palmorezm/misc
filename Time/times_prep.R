429b178c98ffefd728c01f94a0cb2927e2e85051
Zach <70336307+palmorezm@users.noreply.github.com>
2022-12-02 16:08:51 -0600

    Update times_prep.R

--- Time/times_prep.R
+++ Time/times_prep.R
@@ -24,6 +24,7 @@ glink <- paste0("https://docs.google.com/spreadsheets/",
 "AIJo9ejpN902Yzv5Gqetbq3QX5DqjOddVT3caxxftuii/pub?",
 "gid=343744476&single=true&output=csv")
 times <- read.delim(glink, header = T, sep = ",")
+times$Date <- mdy(times$Date) # Does not work if a single failure occurs
 #times$Date <- mdy(times$Date)
 #sort(as_datetime(times$Date))
 # times$Date
@@ -37,15 +38,9 @@ times <- read.delim(glink, header = T, sep = ",")
 #min(as.Date(times$Date))
 #sort(times$Date)[[1]]
 #times$Date[[length(times$Date)]]
-sort(times$Date)[[1]]
-sort(mdy(times$Date), na.rm = T)[[1]]
-seq((sort(mdy(times$Date), na.rm = T)[[1]]), 
-    (times$Date[[length(times$Date)]]), by = "7 days")
-work_week_startdate <- seq(
-  (as_date(sort(times$Date)[[1]])), 
-  (times$Date[[length(times$Date)]]), 
-    by = "7 days")
-times$Date[[length(times$Date)]]
+# Check function of work week start date first (examine parsing)
+work_week_startdate <- seq((sort(times$Date)[[1]]), 
+    times$Date[[length(times$Date)]], by = "7 days")
 times$Start <- hms::as_hms(lubridate::parse_date_time(times$Start, "%I:%M %p"))
 times$End <- hms::as_hms(lubridate::parse_date_time(times$End, "%I:%M %p"))
 times$project_duration <- (times$End - times$Start)
@@ -62,9 +57,9 @@ work_duration_week <- work_duration_day * 5 # 5 days per work week (excluding ho
 # What to do about missing days or days when no project time is entered? 
 # Need an estimator or simulation with the expected normal work days
 # From the start date when data was first entered (3/21) create a sequence of dates every 7 days
-work_week_startdate <- seq(
-  as_date(min(times$Date)), as_date(max(times$Date)), by = "7 days"
-) 
+# work_week_startdate <- seq(
+#   as_date(min(times$Date)), as_date(max(times$Date)), by = "7 days"
+# ) 
 # Create a df with the first work week
 df_all <- data.frame(Date = seq(work_week_startdate[[1]], 
                                      work_week_startdate[[1]] + 4, by = "day"))
@@ -75,7 +70,6 @@ for (i in 2:length(work_week_startdate)){
                                    work_week_startdate[[i]] + 4, by = "day"))
   df_all <- rbind(df_all, df)
 }
-
 df_all <- df_all %>% 
   mutate(time_day = as.integer(work_duration_day)) 
 # Not needed with join via data.table? 
@@ -434,5 +428,3 @@ ggVennDiagram(w,
 # scale_fill_distiller(palette = "Blues", direction = 1)  
 
 ?guides()
-
-
