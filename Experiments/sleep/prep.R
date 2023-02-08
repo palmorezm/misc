9b4ce505da7cf290a515939b0ffd1fa0aa6a70cf
Zach <70336307+palmorezm@users.noreply.github.com>
2023-02-08 10:59:07 -0600

    Cleanup in Prep.R

--- Experiments/sleep/prep.R
+++ Experiments/sleep/prep.R
@@ -9,11 +9,9 @@ d2022 <- readxl::read_xlsx("2022.xlsx")
 # bind the rows of the columns with same name
 # change data types
 
-colnames(d2022)
 d2022 <- d2022[,c(1:3, 9)] # Select times related to sleep each night
 d2021 <- d2021[,c(1:3, 9)] # For both data sets
 colnames(d2021) <- colnames(d2022)
-colnames(d2021)
 df <- rbind(d2021, d2022)
 sum(is.na(df))
 df[which(is.na(df)),]
