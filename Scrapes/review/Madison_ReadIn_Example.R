adb2836ecb5bf6763475d923ad8aeefff19be017
Zach <zacharypalmore@gmail.com>
2023-02-20 11:30:04 -0600

    Mad Read In with New Gnat Stuff

--- Scrapes/review/Madison_ReadIn_Example.R
+++ Scrapes/review/Madison_ReadIn_Example.R
@@ -14,18 +14,24 @@ MadAll <- read.csv(paste0("exports/winter/", MadisonFiles[[1]]), header = TRUE) 
 results <-  read.csv(paste0("exports/winter/", MadisonFiles[[1]]), header = TRUE) %>% 
   mutate(FileName = MadisonFiles[[1]], 
          Date_of_scrape = Sys.Date() - (length(MadisonFiles) - 1))
+Mad1 <- MadAll 
 
 for (i in 2:length(MadisonFiles)){
   # Make a loop through the list of files with Madison in the name
-  results <- read.csv(paste0("exports/winter/", MadisonFiles[[i]]), header = TRUE) %>% 
+  Mad1 <- read.csv(paste0("exports/winter/", MadisonFiles[[i]]), header = TRUE) %>% 
     mutate(FileName = MadisonFiles[[i]], 
            Date_of_scrape = Sys.Date() - (length(MadisonFiles) - i))
   # reassign them to a data frame 
-  MadAll <- rbind(MadAll, results)
+  MadAll <- rbind(MadAll, Mad1)
   # keep the file name so we know where each observation came from 
   # append a the date it was read in to the file
   # add an indicator of progress
 }
+
+# Mad Review
+length(unique(MadAll$Page)) # 11 Scrapes total
+unique(MadAll$Company_Name)
+unique(MadAll$Location_Name)
 
 # Repeat for each of the locations
 # Ignore the company list (that's for Mel to do a quick scan)
@@ -46,4 +52,20 @@ AllFiles <- list(MadisonFiles, MilwaukeeFiles, ChicagoFiles)
 # Remove the stuff that contains "nextmorning" 
 # Keep only the stuff that has the location then a number
 
+results <-  read.csv(paste0("exports/winter/", AllFiles[[1]]), header = TRUE) %>% 
+  mutate(FileName = AllFiles[[1]], 
+         Date_of_scrape = Sys.Date() - (length(AllFiles) - 1))
+resultsAll <- results
+
+for (i in 2:length(AllFiles)){
+  # Make a loop through the list of files with Madison in the name
+  results <- read.csv(paste0("exports/winter/", MadisonFiles[[i]]), header = TRUE) %>% 
+    mutate(FileName = MadisonFiles[[i]], 
+           Date_of_scrape = Sys.Date() - (length(MadisonFiles) - i))
+  # reassign them to a data frame 
+  MadAll <- rbind(MadAll, results)
+  # keep the file name so we know where each observation came from 
+  # append a the date it was read in to the file
+  # add an indicator of progress
+}
 
