ad3a0ba828392a15f65652e95666d7ea62b6e89d
Zach <70336307+palmorezm@users.noreply.github.com>
2023-06-01 15:28:00 -0500

    Descriptive Headers

--- Doulas/realistic_care/app.py
+++ Doulas/realistic_care/app.py
@@ -1,19 +1,19 @@
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    
+    ui.h3("Realistic Doula Care Scenario"),
     # Results
     ui.row(
         # Inputs
         ui.column(6,
             ui.input_slider("n_doulas", "Doulas Available", 0, 100, 6), 
             ui.input_slider("bpd", "Births Per Doula (Monthly)", 0, 5, 1), 
-            ui.input_slider("pbr", "Preterm Birth Rate (%)", 0, 50, 21.16),
-            ui.input_slider("r", "Preterm Reduction Rate (%)", 0, 10, 1.6),
-            ui.input_slider("mgp", "Preterm Average Gest.", 20, 37, 35.52),
-            ui.input_numeric("full_term", "Full Term (Weeks)", 37),
-            ui.input_numeric("cost_per_day", "Daily Preterm Cost", 3000),), 
+            ui.input_slider("pbr", "Preterm Birth Rate (%)", 0, 50, 21.16, step=0.1),
+            ui.input_slider("r", "Preterm Reduction Rate (%)", 0, 10, 1.6, step=0.1),
+            ui.input_slider("mgp", "Preterm Average Gest.", 20, 37, 35.52, step=0.1)), 
         ui.column(6, 
+            ui.input_numeric("full_term", "Full Term (Weeks)", 37),
+            ui.input_numeric("cost_per_day", "Daily Preterm Cost", 3000),
             ui.output_text_verbatim("possible_births"),
             ui.output_text_verbatim("preterm_realbirths"), 
             ui.output_text_verbatim("preterm_realavoided"), 
@@ -31,14 +31,14 @@ def server(input, output, session):
     @output
     @render.text
     def possible_births():
-        return(f"{input.n_doulas()*input.bpd()*12} Doulas Supported Births Possible") # births with doula care
+        return(f"{input.n_doulas()*input.bpd()*12} Doula Supported Births") # births with doula care
 
     # Total Preterm Births 
         # From Doula-Supported Population
     @output
     @render.text
     def preterm_realbirths():
-        return(f"{input.n_doulas()*input.bpd()*12*(input.pbr()/100)} Preterm Births with Doulas") # births before 37 weeks
+        return(f"{round(input.n_doulas()*input.bpd()*12*(input.pbr()/100), 0)} Preterm Births with Doulas") # births before 37 weeks
     
     # Total Expenses Avoided 
         # From Reduction in Preterm Births
@@ -49,14 +49,14 @@ def server(input, output, session):
         total_preterm_normal = (bpy*input.pbr()) 
         total_preterm_doula = bpy*(input.pbr()-(input.r()/100))
         avoided_preterm = total_preterm_normal - total_preterm_doula
-        return(f"{round(avoided_preterm, 2)} Births Avoided Preterm")
+        return(f"{round(avoided_preterm, 2)} Births Prevented from Preterm")
 
     # Mean Gestation Period
     @output
     @render.text
     def preterm_gest():
         days = (input.full_term()*7)-(input.mgp()*7) # 37 weeks = full gest_
-        return(f"{days} Days Preterm on Average") # answer in days
+        return(f"{days} Days on Average Born Before Full Term") # answer in days
 
     # Avoided Costs Considering Doula Availability
     @output
@@ -65,7 +65,7 @@ def server(input, output, session):
         bpy = (input.n_doulas()*input.bpd()*12)
         avoided = (bpy*input.pbr()) - (bpy*(input.pbr()-(input.r()/100)))
         average_days_preterm = (input.full_term()*7)-(input.mgp()*7)
-        avoidedcost = avoided*input.cost_per_day()*average_days_preterm
-        return(f"{round(avoidedcost, 0)} Costs Avoided from Reduction in Preterm Births")
+        avoidedcost = round(avoided*input.cost_per_day()*average_days_preterm, 0)
+        return(f"${avoidedcost:,} Savings from Reduction in Preterm Births")
 
 app = App(app_ui, server)
--- Doulas/universal_care/app.py
+++ Doulas/universal_care/app.py
@@ -1,9 +1,10 @@
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
+    ui.h3("Best-Case Doula Care Scenario"),
     ui.input_numeric("preterm_births", "Number Babies Preterm", 349),
-    ui.input_numeric("total_births", "Number Babies Preterm", 1649),
-    ui.input_numeric("preterm_reduction_rate", "Preterm Reduction Rate (%)", 1.6),
+    ui.input_numeric("total_births", "Number of Births", 1649),
+    ui.input_numeric("preterm_reduction_rate", "Preterm Reduction Rate (%)", 1.6, step=0.1),
     ui.input_slider("normal_birth_cost", "Cost of Birth (Normal, No Complications)", 10000, 50000, 10000),
     ui.output_text_verbatim("preterm_universalavoided"),
     ui.output_text_verbatim("preterm_universalnormalbirthcost"), 
@@ -16,24 +17,24 @@ def server(input, output, session):
     @render.text
     def preterm_universalavoided():
         new_r = (input.preterm_births()/input.total_births())- (input.preterm_reduction_rate() / 100)
-        new_p = input.total_births()*new_r
-        return(new_p)
+        new_p = round(input.total_births()*new_r, 0)
+        return(f'{new_p} Births Before Full Term and {round(input.preterm_births()-new_p, 0)} Babies Prevented From Preterm with Doulas')
 
     @output
     @render.text
     def preterm_universalnormalbirthcost():
         c_norm = input.normal_birth_cost()*(input.total_births() - input.preterm_births())
         c_p = (input.normal_birth_cost()*4)*(input.preterm_births())
-        c_total = c_norm + c_p 
-        return(c_total)
+        c_total = round(c_norm + c_p, 0) 
+        return(f'${c_total:,} Total Medical Costs of All Non-Doula Births')
 
     @output
     @render.text
     def preterm_universaldoulabirthcost():
         c_norm_doula = input.normal_birth_cost()*(input.total_births()-(((input.preterm_births()/input.total_births())-(input.preterm_reduction_rate()/100))*input.total_births()))
         c_p_doula = (input.normal_birth_cost()*4)*(((input.preterm_births()/input.total_births())-(input.preterm_reduction_rate()/100))*input.total_births())
-        c_total_doula = c_norm_doula + c_p_doula
-        return(c_total_doula)
+        c_total_doula = round(c_norm_doula + c_p_doula, 0)
+        return(f'${c_total_doula:,} Total Medical Costs of All Doula Births')
     
     @output
     @render.text
@@ -47,7 +48,7 @@ def server(input, output, session):
         c_p_doula = (input.normal_birth_cost()*4)*(((input.preterm_births()/input.total_births())-(input.preterm_reduction_rate()/100))*input.total_births())
         c_total_doula = c_norm_doula + c_p_doula
         # Difference Normal and Doula Costs
-        c_total_difference = c_total - c_total_doula
-        return(c_total_difference)
+        c_total_difference = round(c_total - c_total_doula, 0)
+        return(f'${c_total_difference:,} Savings from Doula Reduction in Preterm Births')
 
 app = App(app_ui, server)
