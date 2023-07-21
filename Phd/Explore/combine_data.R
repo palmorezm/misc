e22b56cf3e417d53d372c20732c8ede43d5ffca0
Zach <70336307+palmorezm@users.noreply.github.com>
2023-07-21 15:29:34 -0500

    create project for exploration

---
+++ Phd/Explore/combine_data.R
@@ -0,0 +1,13 @@
+
+library(readxl)
+library(dplyr)
+
+response1 <- read_xlsx("Phd Links.xlsx", sheet = 3)
+response2 <- read_xlsx("Phd Links.xlsx", sheet = 4)
+response3 <- read_xlsx("Phd Links.xlsx", sheet = 5)
+response4 <- read_xlsx("Phd Links.xlsx", sheet = 6)
+response5 <- read_xlsx("Phd Links.xlsx", sheet = 7)
+response6 <- read_xlsx("Phd Links.xlsx", sheet = 8)
+response7 <- read_xlsx("Phd Links.xlsx", sheet = 9)
+
+response1
